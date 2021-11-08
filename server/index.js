e47e1225c66aec7b4e7912ca036ba6e2fc853959
Ayan <ayaanestlydontknow@gmail.com>
2021-11-07 22:43:52 -0600

    k

--- server/index.js
+++ server/index.js
@@ -19,7 +19,18 @@ app.get("/api/compliment", (req, res) => {
   let randomCompliment = compliments[randomIndex];
 
   res.status(200).send(randomCompliment);
+
+
+app.get("/api/fortune", (req,res) => {
+  const fortunes = ["A beautiful, smart, and loving person will be coming into your life.","A fresh start will put you on your way.", "A smile is your personal welcome mat.", "A soft voice may be awfully persuasive.", "Adventure can be real happiness.", ];
+  
+  let randomIndex2 = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[randomIndex2];
+  
+  res.status(200).send(randomFortune);
   
 });
+
+
 
 app.listen(4000, () => console.log("Server running on 4000"));
